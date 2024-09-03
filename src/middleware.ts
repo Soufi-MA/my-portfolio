efe0e2ab42388cd5cbe448c7f91682aa367231f7
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-09-03 16:31:49 +0300

    feat: ignore selected files

--- src/middleware.ts
+++ src/middleware.ts
@@ -34,5 +34,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
