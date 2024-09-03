536fcdbfa47ca81ef623443f9783e6363362c734
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-09-03 16:55:33 +0300

    feat: redisign the theme toggle

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -4,36 +4,22 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
+import { useEffect, useState } from "react";
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, theme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button className="aspect-square" variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant={"outline"}
+      size={"icon"}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {mounted && theme === "dark" ? <Moon size={18} /> : <Sun size={18} />}
+    </Button>
   );
 }
