1d83b341b7a9e78c76910addc8850ad872594808
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-09-03 16:56:58 +0300

    fix: refactor dictionaries

--- src/lib/dictionaries/ar.ts
+++ src/lib/dictionaries/ar.ts
@@ -1,27 +1,14 @@
-import {
-  SiExpress,
-  SiGit,
-  SiGo,
-  SiJavascript,
-  SiMongodb,
-  SiNextdotjs,
-  SiNextui,
-  SiNodedotjs,
-  SiPostgresql,
-  SiPython,
-  SiReact,
-  SiRedis,
-  SiTailwindcss,
-  SiThreedotjs,
-  SiTypescript,
-} from "react-icons/si";
-
 export const ar = {
   headers: {
-    about: "نبذة مختصرة",
-    stacks: "التقنيات",
+    home: "الرئيسية",
     projects: "المشاريع",
+    about: "نبذة مختصرة",
     contacts: "وسائل التواصل",
+    stacks: "التقنيات",
+  },
+  hero: {
+    headline: "مرحبًا, أنا محمود",
+    subheadline: "مطور ويب",
   },
   developer: {
     name: "محمود الصوفي",
@@ -30,98 +17,6 @@ export const ar = {
   },
   about:
     "أنا محمود، خريج علوم الكمبيوتر ومقيم في أديس أبابا، إثيوبيا، ولدي أكثر من 4 سنوات من الخبرة في صياغة تجارب الويب الديناميكية. بدأت رحلتي بأساسيات PHP، لكن شغفي بالابتكار قادني إلى استكشاف الأنظمة البيئية React وNext.js، حيث أقوم الآن ببناء تطبيقات تفاعلية وعالية الأداء.",
-  stacks: [
-    {
-      id: 1,
-      name: "رياكت",
-      icon: SiReact,
-      color: "cyan",
-    },
-    {
-      id: 2,
-      name: "نيكست",
-      icon: SiNextdotjs,
-      color: "white",
-    },
-    {
-      id: 3,
-      name: "اكبرس",
-      icon: SiExpress,
-      color: "red",
-    },
-    {
-      id: 4,
-      name: "نود",
-      icon: SiNodedotjs,
-      color: "green",
-    },
-    {
-      id: 5,
-      name: "بوستقرس",
-      icon: SiPostgresql,
-      color: "cyan",
-    },
-    {
-      id: 6,
-      name: "مونجو",
-      icon: SiMongodb,
-      color: "green",
-    },
-    {
-      id: 7,
-      name: "ريديس",
-      icon: SiRedis,
-      color: "red",
-    },
-    {
-      id: 8,
-      name: "جافاسكربت",
-      icon: SiJavascript,
-      color: "yellow",
-    },
-    {
-      id: 9,
-      name: "تايبسكربت",
-      icon: SiTypescript,
-      color: "cyan",
-    },
-    {
-      id: 10,
-      name: "جولانج",
-      icon: SiGo,
-      color: "cyan",
-    },
-    {
-      id: 11,
-      name: "بايثون",
-      icon: SiPython,
-      color: "blue",
-    },
-    {
-      id: 12,
-      name: "تايلويند",
-      icon: SiTailwindcss,
-      color: "cyan",
-    },
-    {
-      id: 13,
-      name: "شاد سي ان",
-      icon: SiNextui,
-      color: "white",
-    },
-    {
-      id: 14,
-      name: "ثري جي اس",
-      icon: SiThreedotjs,
-      color: "white",
-    },
-    {
-      id: 15,
-      name: "جيت",
-      icon: SiGit,
-      color: "red",
-    },
-  ],
   contacts: [
     {
       header: "البريد الإلكتروني:",
--- src/lib/dictionaries/en.ts
+++ src/lib/dictionaries/en.ts
@@ -1,27 +1,14 @@
-import {
-  SiExpress,
-  SiGit,
-  SiGo,
-  SiJavascript,
-  SiMongodb,
-  SiNextdotjs,
-  SiNextui,
-  SiNodedotjs,
-  SiPostgresql,
-  SiPython,
-  SiReact,
-  SiRedis,
-  SiTailwindcss,
-  SiThreedotjs,
-  SiTypescript,
-} from "react-icons/si";
-
 export const en = {
   headers: {
-    about: "About",
-    stacks: "Stacks",
+    home: "Home",
     projects: "Projects",
+    about: "About",
     contacts: "Contacts",
+    stacks: "Stacks",
+  },
+  hero: {
+    headline: "Hi, I'm Soufi",
+    subheadline: "Fullstack Developer",
   },
   developer: {
     name: "Mahmoud Soufi",
@@ -30,98 +17,6 @@ export const en = {
   },
   about:
     "I'm Mahmoud, a Computer Science graduate based in Addis Ababa, Ethiopia, with over 4 years of experience crafting dynamic web experiences. My journey began with the fundamentals of PHP, but my passion for innovation led me to explore the React and Next.js ecosystems, where I now build interactive and performant applications.",
-  stacks: [
-    {
-      id: 1,
-      name: "React",
-      icon: SiReact,
-      color: "cyan",
-    },
-    {
-      id: 2,
-      name: "Next.js",
-      icon: SiNextdotjs,
-      color: "white",
-    },
-    {
-      id: 3,
-      name: "Express",
-      icon: SiExpress,
-      color: "red",
-    },
-    {
-      id: 4,
-      name: "Node.js",
-      icon: SiNodedotjs,
-      color: "green",
-    },
-    {
-      id: 5,
-      name: "PostgreSQL",
-      icon: SiPostgresql,
-      color: "cyan",
-    },
-    {
-      id: 6,
-      name: "MongoDB",
-      icon: SiMongodb,
-      color: "green",
-    },
-    {
-      id: 7,
-      name: "Redis",
-      icon: SiRedis,
-      color: "red",
-    },
-    {
-      id: 8,
-      name: "JavaScript",
-      icon: SiJavascript,
-      color: "yellow",
-    },
-    {
-      id: 9,
-      name: "TypeScript",
-      icon: SiTypescript,
-      color: "cyan",
-    },
-    {
-      id: 10,
-      name: "Golang",
-      icon: SiGo,
-      color: "cyan",
-    },
-    {
-      id: 11,
-      name: "Python",
-      icon: SiPython,
-      color: "blue",
-    },
-    {
-      id: 12,
-      name: "Tailwind CSS",
-      icon: SiTailwindcss,
-      color: "cyan",
-    },
-    {
-      id: 13,
-      name: "Shadcn UI",
-      icon: SiNextui,
-      color: "white",
-    },
-    {
-      id: 14,
-      name: "Three.js",
-      icon: SiThreedotjs,
-      color: "white",
-    },
-    {
-      id: 15,
-      name: "Git",
-      icon: SiGit,
-      color: "red",
-    },
-  ],
   contacts: [
     {
       header: "Email:",
