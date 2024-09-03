68749fb29ea51160c793f32c2e64a10600d63c01
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-09-03 16:53:50 +0300

    feat: refactor home page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,9 +1,6 @@
-import About from "@/components/About";
-import Contacts from "@/components/Contacts";
-import Header from "@/components/Header";
-import Projects from "@/components/Projects";
-import Stacks from "@/components/Stacks";
+import Hero from "@/components/Hero";
 import { getDictionary } from "./dictionaries";
+import { cn } from "@/lib/utils";
 
 export default async function Home({
   params: { lang },
@@ -12,12 +9,20 @@ export default async function Home({
 }) {
   const dict = await getDictionary(lang);
   return (
-    <div className="flex flex-col max-w-[720px] gap-8 w-full px-4 py-16 mx-auto">
-      <Header dict={dict} lang={lang} />
-      <About dict={dict} lang={lang} />
-      <Stacks dict={dict} lang={lang} />
-      <Projects dict={dict} lang={lang} />
-      <Contacts dict={dict} lang={lang} />
+    <div className="flex flex-col gap-8 w-full min-h-screen">
+      <span
+        style={{
+          background: "conic-gradient(from 2.35rad,#4ecdc4,#0658f6)",
+        }}
+        className={cn(
+          "absolute top-[25%] w-[40%] h-1/4 blur-[100px] -z-10 opacity-30 dark:opacity-25",
+          {
+            "left-0": lang === "en",
+            "right-0": lang === "ar",
+          }
+        )}
+      ></span>
+      <Hero dict={dict} lang={lang} />
     </div>
   );
 }
