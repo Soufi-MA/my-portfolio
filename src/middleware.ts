f51b86692b195f9cb538a5ffe0eb7abfbece699f
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-09-03 16:32:00 +0300

    chore: remove comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,7 +17,6 @@ const getLocale = (request: NextRequest) => {
 };
 
 export function middleware(request: NextRequest) {
-  // Check if there is any supported locale in the pathname
   const { pathname } = request.nextUrl;
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
@@ -25,11 +24,8 @@ export function middleware(request: NextRequest) {
 
   if (pathnameHasLocale) return;
 
-  // Redirect if there is no locale
   const locale = getLocale(request);
   request.nextUrl.pathname = `/${locale}${pathname}`;
-  // e.g. incoming request is /products
-  // The new URL is now /en-US/products
   return NextResponse.redirect(request.nextUrl);
 }
 
