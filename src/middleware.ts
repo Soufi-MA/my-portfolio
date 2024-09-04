e5710ddbf214b583913832d22d5f4cbe9af3ad7a
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-09-04 19:11:56 +0300

    fix: exclude static files from middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -32,6 +32,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|static|.*\\..*).*)",
   ],
 };
