8e11754c5916b2bd81aee72b20355e681ea23f35
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-09-05 17:32:15 +0300

    feat: adjust projects grid and project cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,9 @@
 import { getDictionary } from "@/app/[lang]/dictionaries";
 import { cn } from "@/lib/utils";
-import { HashIcon } from "lucide-react";
+import { ExternalLink } from "lucide-react";
 import Image from "next/image";
+import { buttonVariants } from "./ui/button";
+import Link from "next/link";
 
 interface Project {
   id: number;
@@ -12,69 +14,6 @@ interface Project {
   url: string;
 }
 
-const projects: Project[] = [
-  {
-    id: 1,
-    title: "Kabu",
-    thumbnail: "/projects/kabu_thumbnail.png",
-    body: "Revolutionize the way you source furniture for your creative projects. An innovative platform built specifically for designers, architects, and engineers, offering a seamless experience to find, explore, and source the perfect furniture for your vision.",
-    stack: [
-      "Next.js",
-      "Tailwind CSS",
-      "Three.js",
-      "MongoDB",
-      "WebGL",
-      "PayloadCMS",
-    ],
-    url: "https://test.kabu.et/",
-  },
-  {
-    id: 2,
-    title: "Ethiopian Railways",
-    thumbnail: "/projects/railways_thumbnail.png",
-    body: "Experience the convenience of modern rail travel with the Ethiopian Railways online ticket reservation system, built with cutting-edge technologies for a smooth and secure journey.",
-    stack: [
-      "Next.js",
-      "Tailwind CSS",
-      "Express",
-      "PostgreSQL",
-      "NextAuth",
-      "Stripe",
-    ],
-    url: "https://ethiopian-railways.onrender.com",
-  },
-  {
-    id: 3,
-    title: "Harmony",
-    thumbnail: "/projects/harmony_thumbnail.png",
-    body: "Experience the convenience of modern rail travel with the Ethiopian Railways online ticket reservation system, built with cutting-edge technologies for a smooth and secure journey.",
-    stack: [
-      "Next.js",
-      "Tailwind CSS",
-      "Express",
-      "PostgreSQL",
-      "NextAuth",
-      "Stripe",
-    ],
-    url: "https://ethiopian-railways.onrender.com",
-  },
-  {
-    id: 4,
-    title: "Tribond Trading",
-    thumbnail: "/projects/tribond_thumbnail.png",
-    body: "Experience the convenience of modern rail travel with the Ethiopian Railways online ticket reservation system, built with cutting-edge technologies for a smooth and secure journey.",
-    stack: [
-      "Next.js",
-      "Tailwind CSS",
-      "Express",
-      "PostgreSQL",
-      "NextAuth",
-      "Stripe",
-    ],
-    url: "https://ethiopian-railways.onrender.com",
-  },
-];
-
 const Projects = ({
   dict,
   lang,
@@ -82,31 +21,47 @@ const Projects = ({
   dict: Awaited<ReturnType<typeof getDictionary>>;
   lang: "ar" | "en";
 }) => {
+  const ProjectCard = ({
+    project,
+  }: {
+    project: Awaited<ReturnType<typeof getDictionary>>["projects"][0];
+  }) => (
+    <div className="relative w-full border rounded-xl h-[480px] overflow-hidden group cursor-pointer">
+      <Link href={`/${lang}/projects/${project.id}`}>
+        <h2 className="absolute top-4 left-4 right-4 z-10 text-4xl text-center text-white font-semibold">
+          {project.title}
+        </h2>
+        <Image
+          className="h-full w-full rounded-xl group-hover:scale-110 transition-all duration-200 ease-in object-cover"
+          src={project.thumbnail}
+          alt=""
+          height={480}
+          width={660}
+        />
+      </Link>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href={project.url}
+        className={cn(
+          buttonVariants({ size: "icon" }),
+          "absolute bottom-4 right-4 z-10 rounded-full text-black bg-white hover:bg-white/80"
+        )}
+      >
+        <ExternalLink />
+      </a>
+    </div>
+  );
+
   return (
     <section id="projects" className="container flex flex-col gap-8">
-      <h2 className="text-center text-4xl">Selected Projects</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {projects.map((project) => (
+        {dict.projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
     </section>
   );
 };
-
-const ProjectCard = ({ project }: { project: Project }) => (
-  <div className="relative w-full border rounded-xl h-[480px] overflow-hidden group cursor-pointer">
-    <h2 className="absolute top-4 left-4 right-4 z-10 text-4xl text-center text-white font-semibold">
-      {project.title}
-    </h2>
-    <Image
-      className="h-full w-full rounded-xl group-hover:scale-110 transition-all duration-200 ease-in object-cover"
-      src={project.thumbnail}
-      alt=""
-      height={480}
-      width={660}
-    />
-  </div>
-);
 
 export default Projects;
