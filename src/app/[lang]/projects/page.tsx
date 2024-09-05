fefd31c8736715434ca870dffb56e355c9faa1d2
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-09-05 17:31:16 +0300

    feat: new projects page

---
+++ src/app/[lang]/projects/page.tsx
@@ -0,0 +1,19 @@
+import Projects from "@/components/Projects";
+import React from "react";
+import { getDictionary } from "../dictionaries";
+
+const page = async ({
+  params: { lang },
+}: {
+  params: { lang: "en" | "ar" };
+}) => {
+  const dict = await getDictionary(lang);
+
+  return (
+    <div className="pt-28">
+      <Projects dict={dict} lang={lang} />
+    </div>
+  );
+};
+
+export default page;
